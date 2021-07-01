b77fdeef9142a84affa4a8562b0b3b4a42e01cd3
CarolBarbero <Caro_barbero@hotmail.com>
2021-07-01 17:45:35 -0400

    Second Commit

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Gatitos' });
+});
+
+router.get('/nosotros', (req, res) => {
+  res.render('pages/nosotros');
+});
+
+router.get('/contacto', (req, res) => {
+  res.render('pages/contacto');
 });
 
 module.exports = router;
